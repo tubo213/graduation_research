b9796225d32e6fddd902b6655ae20e98d61cb53a
tubo213 <tubo213>
2022-11-25 01:31:14 +0900

    add cvxpy

--- src/services/postprocess/cluster_with_uncertainty_postprocess.py
+++ src/services/postprocess/cluster_with_uncertainty_postprocess.py
@@ -1,65 +1,69 @@
-import multiprocessing
-
+import cvxpy as cvx
 import numpy as np
-import pulp
+import pandas as pd
+from sklearn.cluster import KMeans
+from sklearn.mixture import GaussianMixture
+from sklearn.preprocessing import StandardScaler
+from src.utils import save_as_pickle
 
 
 class ClusterWithUncertaintyPostprocess:
-    def __init__(self, postprocess_config):
+    def __init__(self, base_config, postprocess_config):
+        self.base_config = base_config
         self.postprocess_config = postprocess_config
 
-    def postprocess(self, uplift_mat, budget_constraint, seed=100):
-        customer_list = list(range(uplift_mat.shape[0]))
+    def postprocess(self, uplift_mat, budget_constraint: int, seed: int):
+        # clustering
+        # # normalize uplift_mat
+        clustering_model = self.get_clustering()
+        scaler = StandardScaler()
+        normalized_uplift_mat = scaler.fit_transform(uplift_mat)
+        clustering_model.fit(normalized_uplift_mat)
+        save_clustering_model_path = (
+            self.base_config.dir_config.output_model_dir
+            / f"clustering_model_{seed}_{budget_constraint}.pkl"
+        )
+        save_scaler_path = (
+            self.base_config.dir_config.output_model_dir
+            / f"scaler_{seed}_{budget_constraint}.pkl"
+        )
+        save_as_pickle(clustering_model, save_clustering_model_path)
+        save_as_pickle(scaler, save_scaler_path)
+
+        # # get cluster labels
+        cluster_df = pd.DataFrame(
+            clustering_model.predict(normalized_uplift_mat), columns=["cluster"]
+        )
+        np.save(
+            self.base_config.dir_config.output_optimize_dir
+            / f"cluster_{seed}_{budget_constraint}.npy",
+            cluster_df.to_numpy(),
+        )
+
+        cluster_sizes = cluster_df["cluster"].value_counts().sort_index().to_numpy()
+        uplift_mat = (
+            pd.DataFrame(uplift_mat).groupby(cluster_df["cluster"]).mean().to_numpy()
+        )
+
+        # solve optimization problem
+        cluster_list = list(range(cluster_sizes.shape[0]))
         coupon_list = list(range(uplift_mat.shape[1]))
         customer_coupon_list = [
-            (customer, coupon) for customer in customer_list for coupon in coupon_list
+            (cluster, coupon) for cluster in cluster_list for coupon in coupon_list
         ]
 
-        problem = pulp.LpProblem("CouponAllocation", pulp.LpMaximize)
+        customer_coupon_list
 
-        # 決定変数(x in {0, 1})
-        x = pulp.LpVariable.dicts("x", customer_coupon_list, cat="Binary")
+        return cluster_df["assignment"].to_numpy()
 
-        # 顧客一人に一つ以下のクーポン
-        for customer in customer_list:
-            problem += pulp.lpSum([x[customer, coupon] for coupon in coupon_list]) <= 1
-
-        # 予算制約
-        problem += (
-            pulp.lpSum(
-                [
-                    x[customer, coupon]
-                    * self.postprocess_config.variant_no_to_cost[coupon + 1]
-                    for customer in customer_list
-                    for coupon in coupon_list
-                ]
+    def get_clustering(self):
+        if self.postprocess_config.params["clustering"] == "kmeans":
+            return KMeans(
+                n_clusters=self.postprocess_config.params["n_cluster"],
+                random_state=self.base_config.seed,
             )
-            <= budget_constraint
-        )
-
-        # 目的関数: upliftの総和を最大化
-        problem += pulp.lpSum(
-            [
-                x[customer, coupon] * uplift_mat[customer, coupon]
-                for customer in customer_list
-                for coupon in coupon_list
-            ]
-        )
-
-        # 求解
-        _ = problem.solve(
-            pulp.PULP_CBC_CMD(msg=False, threads=multiprocessing.cpu_count())
-        )
-
-        assginment = [
-            max(
-                [
-                    coupon + 1 if x[customer, coupon].value() == 1 else 0
-                    for coupon in coupon_list
-                ]
+        elif self.postprocess_config.params["clustering"] == "gmm":
+            return GaussianMixture(
+                n_components=self.postprocess_config.params["n_cluster"],
+                random_state=self.base_config.seed,
             )
-            for customer in customer_list
-        ]
-        assert len(assginment) == uplift_mat.shape[0]
-
-        return np.array(assginment)
